1b0090ed3b1ef61cd539d9062c1ee00d847bb4b9
Daniel8541 <106541712+Daniel8541@users.noreply.github.com>
2022-05-30 08:49:03 -0600

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,9 @@
+const numberBtns = document.querySelectorAll('.button');
+const operatorBtns = document.querySelectorAll('.button I');
+const clearBtn = document.querySelector('.button c');
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator
+    })
+})
